ac836ea8e37090066f2629eeeef3c7596c46efd6
somkene12345 <153438701+somkene12345@users.noreply.github.com>
2025-08-11 21:12:20 +0100

    done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,11 +4,11 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import HomeScreen from './index';
 import ExplorePage from './explore';
-import { ThemeProvider } from '../../theme/ThemeContext';
+import { ThemeProvider, useTheme } from '../../theme/ThemeContext';
 
 const Drawer = createDrawerNavigator();
 
-export default function RootLayout() {
+export default function RootLayout( {toggleTheme, darkMode}: any) {
   const { darkMode, toggleTheme } = useTheme();
 
   return (
