c1a03246f5eeafd0aa2e0ab59bddd1d7b7c3103f
somkene12345 <153438701+somkene12345@users.noreply.github.com>
2025-08-27 18:38:54 +0100

    done

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,15 @@
-import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
 import { ThemeProvider } from '../../theme/ThemeContext'; // ✅ import your ThemeProvider
+  import { View, Text } from 'react-native';
+  import { Stack } from 'expo-router';
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
+  export default function Layout() {
+    return (
     <ThemeProvider>
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-          headerShown: false,
-          tabBarButton: HapticTab,
-          tabBarBackground: TabBarBackground,
-          tabBarStyle: Platform.select({
-            ios: {
-              position: 'absolute', // transparent background on iOS
-            },
-            default: {},
-          }),
-        }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: 'Sheet',
-          }}
-        />
-        <Tabs.Screen
-          name="explore"
-          options={{
-            title: 'Analysis',
-          }}
-        />
-      </Tabs>
+      <Stack>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="explore" />
+      </Stack>
     </ThemeProvider>
-  );
-}
+    );
+  }
